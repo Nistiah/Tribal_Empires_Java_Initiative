533ccae6cf44ceb8d2c9a17f668f118b1f05c413
Małgorzata Sieńska <235965@edu.p.lodz.pl>
2022-04-25 23:23:16 +0200

    1. Klasy prowincji

---
+++ Province classes/Babilon.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Babilon {
+    List<String> improvements = Arrays.asList("Świątynia boga Nanny", "Brama Isztar", "wiszące ogrody");
+    List<String> militaryUnit = Arrays.asList("Oddział rydwanów", "Babiloński oddział piechoty");
+    String king = "Kadaszman-Enlil I";
+}
---
+++ Province classes/Kush.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Kush {
+    List<String> improvements = Arrays.asList("Nubijska świątynia", " ");
+    List<String> militaryUnit = Arrays.asList("Kuszyccy łucznicy", "Zmasowany atak", "Słonie bojowe");
+    String king = "Alara";
+}
