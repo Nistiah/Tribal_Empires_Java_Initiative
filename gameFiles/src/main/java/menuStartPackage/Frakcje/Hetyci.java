696fdf6719e8f4db4ea8f99e123eb4ef0f20cdea
Małgorzata Sieńska <235965@edu.p.lodz.pl>
2022-05-10 09:32:02 +0200

    2. Dodana klasa matka Frakcji

--- gameFiles/src/main/java/menuStartPackage/Frakcje/Hetyci.java
+++ gameFiles/src/main/java/menuStartPackage/Frakcje/Hetyci.java
@@ -3,8 +3,14 @@ package menuStartPackage.Frakcje;
 import java.util.Arrays;
 import java.util.List;
 
-public class Hetyci {
+public class Hetyci extends Fraction{
     List<String> improvements = Arrays.asList("Królewska Cytadela", "Zbiorniki Wschodnie", "Brama Królewska");
     List<String> militaryUnit = Arrays.asList("Oddział dwukonnych rydwanów wojennych", "Hetycka piechota uzbrojona w topory");
     String king = "Telepinu";
+
+    public Hetyci() {
+        setImprovements(improvements);
+        setMilitaryUnit(militaryUnit);
+        setKing(king);
+    }
 }
--- gameFiles/src/main/java/menuStartPackage/Frakcje/Persja.java
+++ gameFiles/src/main/java/menuStartPackage/Frakcje/Persja.java
@@ -3,8 +3,14 @@ package menuStartPackage.Frakcje;
 import java.util.Arrays;
 import java.util.List;
 
-public class Persja {
+public class Persja extends Fraction{
     List<String> improvements = Arrays.asList("Apadana w Persepolis", "Ziggurat w Czoga Zanbil");
     List<String> militaryUnit = Arrays.asList("Nieśmiertelni", "Jazda konna");
     String king = "Dejokes";
+
+    public Persja() {
+        setImprovements(improvements);
+        setMilitaryUnit(militaryUnit);
+        setKing(king);
+    }
 }
